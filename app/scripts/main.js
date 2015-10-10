d6d47ca43d434de0c2b7a5f55aa2d4f86198b247
Luke Petrolekas <luke.petrolekas@gmail.com>
2015-10-10 02:28:06 -0400

    add lead unit and normalize.css

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
+var RATIO = 1.618;
+var RATIO4 = RATIO*RATIO*RATIO*RATIO;
+
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+var resize = function(unit, ratio) {
+  var unitName = '[data-c-'+ unit +'=""]';
+  $(unitName).css( "height", Math.floor($(unitName).outerWidth(true) / ratio)  );
+}
+
+
+var resizeAll = function () {
+   resize("lead", RATIO4);
+};
+
+$( window ).bind("resize", function(){
+    resizeAll();
+});
+
+resizeAll();
