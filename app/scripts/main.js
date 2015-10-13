21a49843c6df3fb399867b1667aa8d3ca16327b3
Luke Petrolekas <luke.petrolekas@gmail.com>
2015-10-13 00:04:00 -0400

    Create responsive layout with six subdivisions that break when the subdivision is under 200px

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,19 +2,3 @@ var RATIO = 1.618;
 var RATIO4 = RATIO*RATIO*RATIO*RATIO;
 
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
-
-var resize = function(unit, ratio) {
-  var unitName = '[data-c-'+ unit +'=""]';
-  $(unitName).css( "height", Math.floor($(unitName).outerWidth(true) / ratio)  );
-}
-
-
-var resizeAll = function () {
-   resize("lead", RATIO4);
-};
-
-$( window ).bind("resize", function(){
-    resizeAll();
-});
-
-resizeAll();
